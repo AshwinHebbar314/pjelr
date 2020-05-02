a93fd539385422a2f809d92717d8ca9dfdb967d4
AshwinHebbar314 <64640139+AshwinHebbar314@users.noreply.github.com>
2020-05-02 08:11:53 +0530

    003

---
+++ 003.py
@@ -0,0 +1,39 @@
+#%%
+import numpy as np
+def isEven(n):
+    if n&1:
+        return False
+    else:
+        return True
+
+def isFactor(i,n):
+  if n%i == 0:
+    return True
+  else:
+    return False
+
+def isPrime(n):
+    if (n <= 1): 
+        return False
+    if (n <= 3): 
+        return True
+    if (n % 2 == 0 or n % 3 == 0): 
+        return False
+    i = 5
+    while(i * i <= n) : 
+        if (n%i == 0 or n%(i+2)==0) : 
+          return False
+        i = i + 6
+    return True
+
+def prime_factor(n):
+  l1 = []
+  for i in range(int(round(np.sqrt(n)))):
+    if(i*i <= n):
+      if isEven(i) == False:
+        if isPrime(i):
+          if isFactor(i,n):
+            l1.append(i)
+  return np.max(l1)
+
+prime_factor(600851475143)
